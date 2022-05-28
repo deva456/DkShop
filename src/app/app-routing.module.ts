fe8adc565af58ba72b51253aa4c329be96ca8bc6
Bhanu Bediya <bhanu15599@mastek.com>
2022-05-28 15:47:56 +0530

    bb-header1-done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ThankyouComponent } from './components/thankyou/thankyou.component';
+
+const routes: Routes = [
+  {
+  path: '',component: HomeComponent
+  },
+  {
+    path: 'cart', component:CartComponent
+  },
+  {
+    path: 'checkout', component: CheckoutComponent
+  },
+  {
+    path: 'product/:id', component: ProductsComponent
+  },
+  {
+    path: 'thankyou', component: ThankyouComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
