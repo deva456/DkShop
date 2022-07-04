0a36c05ed3e2e55ac1247faf61e908e73cb4be5f
Bhanu Bediya <bhanu15599@mastek.com>
2022-07-04 22:09:52 +0530

    backup

--- src/app/components/thankyou/thankyou.component.ts
+++ src/app/components/thankyou/thankyou.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IBillingDetails } from 'src/app/billingDetails';
+import { IProduct } from 'src/app/iproduct';
 import { BillingDetailsService } from 'src/app/services/billing-details.service';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-thankyou',
@@ -9,13 +11,22 @@ import { BillingDetailsService } from 'src/app/services/billing-details.service'
 })
 export class ThankyouComponent implements OnInit {
 bill:IBillingDetails[]=[];
-  constructor( private billingDetails:BillingDetailsService) { }
+public product:IProduct[]=[];
+public grandTotal!:number;
+dateObj= Date.now();
+  constructor( private billingDetails:BillingDetailsService, private cartService:CartService) { }
 
   ngOnInit(): void {
     this.billingDetails.getBillingDetails().subscribe((data:IBillingDetails[])=>{
       console.log(data)
       this.bill=data;
       console.log(this.bill)
+    })
+
+    this.cartService.getProducts()
+    .subscribe(res=>{
+      this.product=res;
+      this.grandTotal=this.cartService.getTotalPrice();
     })
   }
 
